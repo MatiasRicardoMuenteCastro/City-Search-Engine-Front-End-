f1cd92418968cfefbe91f1d11b9fa0ae2e460802
MatiasRicardoMuenteCastro <matiascastro@outlook.com.br>
2022-10-30 02:35:17 -0300

    deploy

---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+    function Main(){
+        $this->HomePage()
+    }
+    function HomePage(){
+        $output = Template("_index.html")
+    }
+>
--- routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-$request = $_SERVER['REQUEST_URI'];
-
-switch ($request) {
-    case '/' :
-        require __DIR__ . '/views/index.html';
-        break;
-}
->
